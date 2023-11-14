e9413cf94fbc7cfdfb8733d9fe03dbb561fb2e70
KristofferRoenLie <KristofferRoenLie@gmail.com>
2023-11-15 00:26:38 +0100

    fdh

--- src/pages/campaign-creator/campaign-creator-page.cmp.ts
+++ src/pages/campaign-creator/campaign-creator-page.cmp.ts
@@ -1,9 +1,23 @@
+import { MMInput } from '@roenlie/mimic-elements/input';
 import { customElement, MimicElement } from '@roenlie/mimic-lit/element';
+import { html } from 'lit';
+
+MMInput.register();
 
 
 @customElement('mh-campaign-creator-page')
 export class CampaignCreatorPage extends MimicElement {
 
 	public static page = true;
+
+
+	protected override render(): unknown {
+		return html`
+		<mm-input
+			label="Campaign name"
+		></mm-input>
+
+		`;
+	}
 
 }
